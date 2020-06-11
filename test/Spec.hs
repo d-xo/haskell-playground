61a58ea089f43934a22cab03a442aa4fc455915a
David Terry <me@xwvvvvwx.com>
2020-06-11 16:18:39 +0200

    applicative laws

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,14 @@
-main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE AllowAmbiguousTypes #-}
+
+import Test.QuickCheck
+
+identityLaw :: (Applicative a, Eq (a b)) => a b -> Bool
+identityLaw x = (pure id <*> x) == x
+
+homomorphismLaw :: (Applicative c) => (a -> b) -> a -> Bool
+homomorphismLaw  f x = (pure f <*> pure x) == (pure (f x) :: c b)
+
+main = do
+  quickCheck (identityLaw :: Maybe Integer -> Bool)
+  --quickCheck (homomorphismLaw :: (Int -> Int) -> Int -> Bool)
